7831cd2055a7e5e2d423fa96e5f17ee1b4e0f30f
minipaulitat <minipaulitat@gmail.com>
2023-01-28 00:45:26 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,121 @@
+
+function english(){
+    document.getElementById("language").innerHTML=`
+    
+
+    </div>
+    
+    <h2 class=" m-4 fw-bolder">Introduction</h2>
+    <div class="row">
+        <div class="col-sm-9 ">
+    <p class="mt-4">My name is Paula Turner, I'm 30 years old and I'm a pastry chef, administrative accountant and I'm currently studying to become a web developer.</p>
+    <p>I have experience in costumer service, accounting and professional cooking.</p>
+    <p>I consider myself to be a proactive person that's always trying to improve and adquire new knowledge</p>
+    <p>Currently, I'm looking for opportunities in the IT world, in order to become a better professional.</p>
+    
+    </div>
+    
+    <img class="col-sm-2  resume-picture" src="./resume.jpg" alt=""  >
+    
+    </div>
+    
+    <div class="bg-blue">
+    <h2 class=" m-4 fw-bolder">Education</h2>
+    <div class="row">
+    
+        <div class="col">
+    <p class="fw-bolder">Facultad de psicología UNR</p>
+    <p>psychology(2011-actualidad)</p>
+    </div>
+    
+    <div class="col">
+    <p class="fw-bolder">Escuela AEHGAR</p>
+    <p>Professional chef (2017-2018)</p>
+    </div>
+    
+    <div class="col">
+    <p class="fw-bolder">Escuela Superior de Comercio</p>
+    <p>Highschool</p>
+    </div>
+    </div>
+    
+    <h2 class=" m-4 fw-bolder">Work experience</h2>
+    
+    <div class="row justify-content-center">
+        <div class="col-sm-8 mb-3 mb-sm-0">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title  fw-bold">CAS volunteer.</h5>
+              <p class="card-text">Telephone assistance (2015-2016).</p>
+             
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-8 mb-3 mb-sm-0">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title  fw-bold">Kitchen helper Hotel Ros Tower.</h5>
+              <p class="card-text">assistant in both savoury and pastry (2017-2018).</p>
+            </div>
+          </div>
+        </div>
+    
+        <div class="col-sm-8 mb-3 mb-sm-0">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title  fw-bold">Chef and pastry chef at Suco</h5>
+                <p class="card-text">In charge of production, suppliers, and stock (2018-2020).</p>
+              </div>
+            </div>
+          </div>
+    
+          <div class="col-sm-8 mb-3 mb-sm-0">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title  fw-bold">Administrative accountant Herramientas Litoral.</h5>
+                <p class="card-text">in charge of suppliers, payments and costumer service (2020-actualidad).</p>
+              </div>
+            </div>
+          </div>
+    
+          <div class="col-sm-8 mb-3 mb-sm-0">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title  fw-bold">Pastry chef at Rollin' cream</h5>
+                <p class="card-text">Pastry production, suppliers and stock handler (2021-actualidad)</p>
+              </div>
+            </div>
+          </div>
+    
+      </div>
+    
+    
+    
+    <h2 class=" m-4 fw-bolder">Skills</h2>
+    
+    <ul>
+        <li>Teamwork</li>
+        <li>lidership</li>
+        <li> multitasking</li>
+        <li>Social network knowledge</li>
+        <li>Profficient in Office suite</li>
+        <li>Familiar with Canva</li>
+        <li>knowledge of HTML, CSS and Javascript</li>
+    </ul>
+    
+    <h2 class="m-4 fw-bolder">Contact Information</h2>
+    
+    <p>Address: Cochabamba 613, Rosario, Argentina</p>
+    <p>Phone: +54 (0341) 6818215</p>
+    <p>Mail: paula.turner.c@gmail.com</p>
+    
+    
+    </div>
+    
+    
+        
+        `;
+
+}
+
+
